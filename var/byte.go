bb0082ae79b33ded8fc9548d28587551f0ed7666
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-16 12:04:50 +0900

    :sparkles: learn rune type

--- var/byte.go
+++ var/byte.go
@@ -12,4 +12,29 @@ func main() {
 
 	array := []byte{41, 2}
 	fmt.Println(array) // [41 2]
+
+	// rune 周り
+	// Unicodeのコードポイントを表す
+	// https://qiita.com/seihmd/items/4a878e7fa340d7963fee
+	// https://zenn.dev/masaruxstudy/articles/52632501e4ca41
+
+	s := "あ"
+
+	for i := 0; i < len(s); i++ {
+		b := s[i] // byte
+		fmt.Println(b)
+	}
+
+	for i := 0; i < len(s); i++ {
+		/* %xで16進数で出力する */
+		fmt.Printf("% x", s[i]) // e3 81 82
+	}
+
+	for _, r := range s {
+		// rune
+		fmt.Println("\nrune")
+		// Unicodeの番号を10進数に変換したもの
+		fmt.Println(r) //12354
+	}
+
 }
