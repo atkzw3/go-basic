34a80d67a604e8dfea8bbb6d638ac1761c68e27d
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-16 17:22:00 +0900

    :sparkles: learn arithmetic operator

---
+++ operator/arithmetic.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 2) // 3
+	fmt.Println(2 - 2) // 0
+	fmt.Println(2 * 2) // 4
+	fmt.Println(2 / 2) // 1
+	fmt.Println(3 % 2) // 1
+
+	fmt.Println("AB" + "CD") // ABCD
+	n := 0
+	n += 4
+	fmt.Println(n) // 4
+	n++
+	fmt.Println(n) // 5
+	n--
+	fmt.Println(n) //4
+
+	s := "abcd"
+	s += "e"
+	fmt.Println(s) // abcde
+
+}
