1dfb263fcf2e06d0707106a5fe0e3a36d8071975
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-19 22:44:42 +0900

    :sparkles: learn slice append , make , len , cap

---
+++ reference/slice2.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	sl := []int{1, 2, 3}
+	fmt.Println(sl) // [1 2 3]
+
+	// append
+	sl = append(sl, 4, 5, 6)
+	fmt.Println(sl) // [1 2 3 4 5 6]
+
+	// make
+	sl2 := make([]int, 3)
+	fmt.Println(sl2) // [0 0 0]
+
+	// len・cap
+	// len => 要素数
+	// cap => 容量
+	fmt.Println(len(sl2), cap(sl2)) // 3 3
+
+	sl3 := make([]int, 3, 5)
+	fmt.Println(sl3)                // [0 0 0]
+	fmt.Println(len(sl3), cap(sl3)) // 3 5
+	// capはメモリなど気にする時に考慮する
+
+	sl3 = append(sl3, 4, 5, 6)
+	fmt.Println(sl3)
+	// 容量が 5から10の2倍になる
+	// ======= 不用意に追加などしてメモリを多く使用しないこと =========
+	fmt.Println(len(sl3), cap(sl3)) // 6 10
+
+}
